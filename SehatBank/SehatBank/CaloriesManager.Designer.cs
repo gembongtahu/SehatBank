0ba51f95aed1e80c4b768f24a963fd75c0564a23
Thoriqutsani Anastyo Rasnan <thoriq.rasnan@gmail.com>
2023-10-31 14:08:57 +0700

    Edit Calories Manager From

--- SehatBank/SehatBank/CaloriesManager.Designer.cs
+++ SehatBank/SehatBank/CaloriesManager.Designer.cs
@@ -28,90 +28,217 @@
         /// </summary>
         private void InitializeComponent()
         {
-            label1 = new Label();
-            mainMenuButton = new Button();
+            managerLabel = new Label();
             addButton = new Button();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            label2 = new Label();
-            label3 = new Label();
+            foodTextBox = new TextBox();
+            caloriesTextBox = new TextBox();
+            foodLabel = new Label();
+            caloriesLabel = new Label();
+            editButton = new Button();
+            deleteButton = new Button();
+            daySevenView = new DataGridView();
+            mainMenuButton = new Button();
+            daySixView = new DataGridView();
+            dayFiveView = new DataGridView();
+            dayFourView = new DataGridView();
+            dayThreeView = new DataGridView();
+            dayTwoView = new DataGridView();
+            dayOneView = new DataGridView();
+            inputBox = new GroupBox();
+            ((System.ComponentModel.ISupportInitialize)daySevenView).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)daySixView).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dayFiveView).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dayFourView).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dayThreeView).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dayTwoView).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dayOneView).BeginInit();
+            inputBox.SuspendLayout();
             SuspendLayout();
             // 
-            // label1
+            // managerLabel
             // 
-            label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            label1.AutoSize = true;
-            label1.Font = new Font("Heebo", 48F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.ForeColor = SystemColors.ButtonHighlight;
-            label1.Location = new Point(140, 52);
-            label1.Name = "label1";
-            label1.Size = new Size(544, 94);
-            label1.TabIndex = 1;
-            label1.Text = "Calories Manager";
-            label1.TextAlign = ContentAlignment.MiddleCenter;
+            managerLabel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            managerLabel.AutoSize = true;
+            managerLabel.Font = new Font("Heebo", 27.75F, FontStyle.Regular, GraphicsUnit.Point);
+            managerLabel.ForeColor = SystemColors.ButtonHighlight;
+            managerLabel.Location = new Point(49, 29);
+            managerLabel.Name = "managerLabel";
+            managerLabel.Size = new Size(314, 55);
+            managerLabel.TabIndex = 13;
+            managerLabel.Text = "Calories Manager";
+            managerLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // addButton
+            // 
+            addButton.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            addButton.BackColor = Color.DimGray;
+            addButton.ForeColor = SystemColors.ButtonHighlight;
+            addButton.Location = new Point(211, 15);
+            addButton.Name = "addButton";
+            addButton.Size = new Size(50, 23);
+            addButton.TabIndex = 2;
+            addButton.Text = "Add";
+            addButton.UseVisualStyleBackColor = false;
+            addButton.Click += addButton_Click;
+            // 
+            // foodTextBox
+            // 
+            foodTextBox.Location = new Point(105, 25);
+            foodTextBox.Name = "foodTextBox";
+            foodTextBox.Size = new Size(100, 23);
+            foodTextBox.TabIndex = 0;
+            // 
+            // caloriesTextBox
+            // 
+            caloriesTextBox.Location = new Point(105, 49);
+            caloriesTextBox.Name = "caloriesTextBox";
+            caloriesTextBox.Size = new Size(100, 23);
+            caloriesTextBox.TabIndex = 1;
+            // 
+            // foodLabel
+            // 
+            foodLabel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            foodLabel.AutoSize = true;
+            foodLabel.Font = new Font("Heebo", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            foodLabel.ForeColor = SystemColors.ButtonHighlight;
+            foodLabel.Location = new Point(17, 26);
+            foodLabel.Name = "foodLabel";
+            foodLabel.Size = new Size(92, 22);
+            foodLabel.TabIndex = 6;
+            foodLabel.Text = "Food Name:";
+            foodLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // caloriesLabel
+            // 
+            caloriesLabel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            caloriesLabel.AutoSize = true;
+            caloriesLabel.Font = new Font("Heebo", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            caloriesLabel.ForeColor = SystemColors.ButtonHighlight;
+            caloriesLabel.Location = new Point(39, 48);
+            caloriesLabel.Name = "caloriesLabel";
+            caloriesLabel.Size = new Size(70, 22);
+            caloriesLabel.TabIndex = 7;
+            caloriesLabel.Text = "Calories:";
+            caloriesLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // editButton
+            // 
+            editButton.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            editButton.BackColor = Color.DimGray;
+            editButton.ForeColor = SystemColors.ButtonHighlight;
+            editButton.Location = new Point(211, 38);
+            editButton.Name = "editButton";
+            editButton.Size = new Size(50, 23);
+            editButton.TabIndex = 3;
+            editButton.Text = "Edit";
+            editButton.UseVisualStyleBackColor = false;
+            editButton.Click += editButton_Click;
+            // 
+            // deleteButton
+            // 
+            deleteButton.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            deleteButton.BackColor = Color.DimGray;
+            deleteButton.ForeColor = SystemColors.ButtonHighlight;
+            deleteButton.Location = new Point(211, 61);
+            deleteButton.Name = "deleteButton";
+            deleteButton.Size = new Size(50, 23);
+            deleteButton.TabIndex = 4;
+            deleteButton.Text = "Delete";
+            deleteButton.UseVisualStyleBackColor = false;
+            deleteButton.Click += deleteButton_Click;
+            // 
+            // daySevenView
+            // 
+            daySevenView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            daySevenView.Location = new Point(55, 130);
+            daySevenView.Name = "daySevenView";
+            daySevenView.RowTemplate.Height = 25;
+            daySevenView.Size = new Size(94, 241);
+            daySevenView.TabIndex = 6;
             // 
             // mainMenuButton
             // 
             mainMenuButton.BackColor = Color.DimGray;
             mainMenuButton.ForeColor = SystemColors.ButtonHighlight;
-            mainMenuButton.Location = new Point(625, 378);
+            mainMenuButton.Location = new Point(681, 403);
             mainMenuButton.Name = "mainMenuButton";
             mainMenuButton.Size = new Size(107, 23);
-            mainMenuButton.TabIndex = 2;
+            mainMenuButton.TabIndex = 5;
             mainMenuButton.Text = "Main Menu";
             mainMenuButton.UseVisualStyleBackColor = false;
             mainMenuButton.Click += mainMenuButton_Click;
             // 
-            // addButton
+            // daySixView
             // 
-            addButton.BackColor = Color.DimGray;
-            addButton.ForeColor = SystemColors.ButtonHighlight;
-            addButton.Location = new Point(286, 225);
-            addButton.Name = "addButton";
-            addButton.Size = new Size(67, 23);
-            addButton.TabIndex = 3;
-            addButton.Text = "Add";
-            addButton.UseVisualStyleBackColor = false;
+            daySixView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            daySixView.Location = new Point(155, 130);
+            daySixView.Name = "daySixView";
+            daySixView.RowTemplate.Height = 25;
+            daySixView.Size = new Size(94, 241);
+            daySixView.TabIndex = 7;
             // 
-            // textBox1
+            // dayFiveView
             // 
-            textBox1.Location = new Point(253, 167);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
-            textBox1.TabIndex = 4;
+            dayFiveView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dayFiveView.Location = new Point(255, 130);
+            dayFiveView.Name = "dayFiveView";
+            dayFiveView.RowTemplate.Height = 25;
+            dayFiveView.Size = new Size(94, 241);
+            dayFiveView.TabIndex = 8;
             // 
-            // textBox2
+            // dayFourView
             // 
-            textBox2.Location = new Point(253, 196);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(100, 23);
-            textBox2.TabIndex = 5;
+            dayFourView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dayFourView.Location = new Point(355, 130);
+            dayFourView.Name = "dayFourView";
+            dayFourView.RowTemplate.Height = 25;
+            dayFourView.Size = new Size(94, 241);
+            dayFourView.TabIndex = 9;
             // 
-            // label2
+            // dayThreeView
             // 
-            label2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            label2.AutoSize = true;
-            label2.Font = new Font("Heebo", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.ForeColor = SystemColors.ButtonHighlight;
-            label2.Location = new Point(159, 167);
-            label2.Name = "label2";
-            label2.Size = new Size(92, 22);
-            label2.TabIndex = 6;
-            label2.Text = "Food Name:";
-            label2.TextAlign = ContentAlignment.MiddleCenter;
+            dayThreeView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dayThreeView.Location = new Point(455, 130);
+            dayThreeView.Name = "dayThreeView";
+            dayThreeView.RowTemplate.Height = 25;
+            dayThreeView.Size = new Size(94, 241);
+            dayThreeView.TabIndex = 10;
             // 
-            // label3
+            // dayTwoView
             // 
-            label3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            label3.AutoSize = true;
-            label3.Font = new Font("Heebo", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
-            label3.ForeColor = SystemColors.ButtonHighlight;
-            label3.Location = new Point(181, 195);
-            label3.Name = "label3";
-            label3.Size = new Size(70, 22);
-            label3.TabIndex = 7;
-            label3.Text = "Calories:";
-            label3.TextAlign = ContentAlignment.MiddleCenter;
+            dayTwoView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dayTwoView.Location = new Point(555, 130);
+            dayTwoView.Name = "dayTwoView";
+            dayTwoView.RowTemplate.Height = 25;
+            dayTwoView.Size = new Size(94, 241);
+            dayTwoView.TabIndex = 11;
+            // 
+            // dayOneView
+            // 
+            dayOneView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dayOneView.Location = new Point(655, 130);
+            dayOneView.Name = "dayOneView";
+            dayOneView.RowTemplate.Height = 25;
+            dayOneView.Size = new Size(94, 241);
+            dayOneView.TabIndex = 12;
+            // 
+            // inputBox
+            // 
+            inputBox.Controls.Add(caloriesTextBox);
+            inputBox.Controls.Add(foodTextBox);
+            inputBox.Controls.Add(foodLabel);
+            inputBox.Controls.Add(caloriesLabel);
+            inputBox.Controls.Add(deleteButton);
+            inputBox.Controls.Add(editButton);
+            inputBox.Controls.Add(addButton);
+            inputBox.ForeColor = SystemColors.ButtonHighlight;
+            inputBox.Location = new Point(476, 12);
+            inputBox.Name = "inputBox";
+            inputBox.Size = new Size(267, 90);
+            inputBox.TabIndex = 17;
+            inputBox.TabStop = false;
+            inputBox.Text = "Input Here";
+            inputBox.Enter += inputBox_Enter;
             // 
             // CaloriesManager
             // 
@@ -119,28 +246,51 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.DimGray;
             ClientSize = new Size(800, 450);
-            Controls.Add(label3);
-            Controls.Add(label2);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
-            Controls.Add(addButton);
+            Controls.Add(inputBox);
+            Controls.Add(dayOneView);
+            Controls.Add(dayTwoView);
+            Controls.Add(dayThreeView);
+            Controls.Add(dayFourView);
+            Controls.Add(dayFiveView);
+            Controls.Add(daySixView);
+            Controls.Add(daySevenView);
             Controls.Add(mainMenuButton);
-            Controls.Add(label1);
+            Controls.Add(managerLabel);
             Name = "CaloriesManager";
             Text = "CaloriesManager";
             Load += CaloriesManager_Load;
+            Resize += CaloriesManager_Resize;
+            ((System.ComponentModel.ISupportInitialize)daySevenView).EndInit();
+            ((System.ComponentModel.ISupportInitialize)daySixView).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dayFiveView).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dayFourView).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dayThreeView).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dayTwoView).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dayOneView).EndInit();
+            inputBox.ResumeLayout(false);
+            inputBox.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private Label label1;
-        private Button mainMenuButton;
+        private Label managerLabel;
         private Button addButton;
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private Label label2;
-        private Label label3;
+        private TextBox foodTextBox;
+        private TextBox caloriesTextBox;
+        private Label foodLabel;
+        private Label caloriesLabel;
+        private Button editButton;
+        private Button deleteButton;
+        private DataGridView daySevenView;
+        private Button mainMenuButton;
+        private DataGridView daySixView;
+        private DataGridView dayFiveView;
+        private DataGridView dayFourView;
+        private DataGridView dayThreeView;
+        private DataGridView dayTwoView;
+        private DataGridView dayOneView;
+        private GroupBox inputBox;
     }
 }
--- SehatBank/SehatBank/CaloriesManager.cs
+++ SehatBank/SehatBank/CaloriesManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,13 @@ namespace SehatBank
 {
     public partial class CaloriesManager : Form
     {
+        private Rectangle mmButton, aButton, eButton, dButton, originalSize;
+        private Rectangle daySeven, daySix, dayFive, dayFour, dayThree, dayTwo, dayOne;
+        private Rectangle mLabel, fLabel, cLabel;
+        private Rectangle fTextBox, cTextBox;
+        private Rectangle iBox;
+        private Font mFont, fFont, cFont;
+
         public CaloriesManager()
         {
             InitializeComponent();
@@ -23,6 +31,85 @@ namespace SehatBank
         }
 
         private void CaloriesManager_Load(object sender, EventArgs e)
+        {
+            originalSize = this.Bounds;
+            mmButton = mainMenuButton.Bounds;
+            aButton = addButton.Bounds;
+            eButton = editButton.Bounds;
+            dButton = deleteButton.Bounds;
+            daySeven = daySevenView.Bounds;
+            dayFive = dayFiveView.Bounds;
+            daySix = daySixView.Bounds;
+            dayOne = dayOneView.Bounds;
+            dayTwo = dayTwoView.Bounds;
+            dayThree = dayThreeView.Bounds;
+            dayFour = dayFourView.Bounds;
+            mLabel = managerLabel.Bounds;
+            fLabel = foodLabel.Bounds;
+            cLabel = caloriesLabel.Bounds;
+            fTextBox = foodTextBox.Bounds;
+            cTextBox = caloriesTextBox.Bounds;
+            iBox = inputBox.Bounds;
+            mFont = managerLabel.Font;
+            fFont = foodLabel.Font;
+            cFont = caloriesLabel.Font;
+        }
+
+        private void resizeControl(Rectangle r, Control c)
+        {
+            float xRatio = (float)(this.Width) / (float)(originalSize.Width), yRatio = (float)(this.Height) / (float)(originalSize.Height);
+            int newX = (int)(r.X * xRatio), newY = (int)(r.Y * yRatio), newWidth = (int)(r.Width * xRatio), newHeight = (int)(r.Height * yRatio);
+            c.Location = new Point(newX, newY);
+            c.Size = new Size(newWidth, newHeight);
+        }
+
+        private void resizeFont(Font f, Control c)
+        {
+            float ratio = (float)this.Height / (float)originalSize.Height;
+            int newSize = (int)(f.Size * ratio);
+            c.Font = new Font(f.FontFamily, newSize, f.Style);
+        }
+
+        private void CaloriesManager_Resize(object sender, EventArgs e)
+        {
+            resizeControl(mmButton, mainMenuButton);
+            resizeControl(aButton, addButton);
+            resizeControl(eButton, editButton);
+            resizeControl(dButton, deleteButton);
+            resizeControl(dayOne, dayOneView);
+            resizeControl(dayTwo, dayTwoView);
+            resizeControl(dayThree, dayThreeView);
+            resizeControl(dayFour, dayFourView);
+            resizeControl(dayFive, dayFiveView);
+            resizeControl(daySix, daySixView);
+            resizeControl(daySeven, daySevenView);
+            resizeControl(mLabel, managerLabel);
+            resizeControl(fLabel, foodLabel);
+            resizeControl(cLabel, caloriesLabel);
+            resizeControl(fTextBox, foodTextBox);
+            resizeControl(cTextBox, caloriesTextBox);
+            resizeControl(iBox, inputBox);
+            resizeFont(cFont, caloriesLabel);
+            resizeFont(fFont, foodLabel);
+            resizeFont(mFont, managerLabel);
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void editButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void inputBox_Enter(object sender, EventArgs e)
         {
 
         }
