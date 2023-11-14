01709dee1af65070222afd9277d9cc6cec41bbe6
Thoriqutsani Anastyo Rasnan <thoriq.rasnan@gmail.com>
2023-11-14 17:15:24 +0700

    Update Get Food And Activities List

--- SehatBank/SehatBank/Food.cs
+++ SehatBank/SehatBank/Food.cs
@@ -1,22 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using Npgsql;
 
 namespace SehatBank
 {
-    internal class Food
+    public class Food
     {
-        public int FoodID { get; }
-        public string FoodName { get; }
-        public int Calories { get; }
-
-        public Food(int foodID, string foodName, int calories)
+        public static List<string> GetFoodList()
         {
-            FoodID = foodID;
-            FoodName = foodName;
-            Calories = calories;
+            string constring = "Host=localhost;Port=5432;Username=postgres;Password=admin;Database=SehatBank";
+            List<string> list = new List<string>();
+            using (NpgsqlConnection connection = new NpgsqlConnection(constring))
+            {
+                try
+                {
+                    connection.Open();
+                    string sql = "SELECT food_name FROM food";
+
+                    using (NpgsqlCommand command = new NpgsqlCommand(sql, connection))
+                    {
+                        using (NpgsqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                string foodName = reader["food_name"].ToString();
+                                list.Add(foodName);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+
+            return list;
         }
     }
 }
