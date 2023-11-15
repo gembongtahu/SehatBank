68f3842fc7c36c20aae33ae7a0583119d5cad3e4
Thoriqutsani Anastyo Rasnan <thoriq.rasnan@gmail.com>
2023-11-16 01:03:38 +0700

    Add CRUD Feature

--- SehatBank/SehatBank/ActivitiesManager.cs
+++ SehatBank/SehatBank/ActivitiesManager.cs
@@ -22,10 +22,10 @@ namespace SehatBank
         private DataGridViewRow r;
         private Rectangle mmButton, aButton, eButton, dButton, originalSize;
         private Rectangle daySeven, daySix, dayFive, dayFour, dayThree, dayTwo, dayOne;
-        private Rectangle mLabel, fLabel, cLabel;
-        private Rectangle fTextBox, cTextBox;
+        private Rectangle mLabel, fLabel;
+        private Rectangle fTextBox;
         private Rectangle iBox;
-        private Font mFont, fFont, cFont;
+        private Font mFont, fFont;
         Resize resize = new Resize();
         List<string> sqlCommandList = new List<string>();
         List<string> activitiesList = new List<string>();
@@ -69,13 +69,10 @@ namespace SehatBank
             dayFour = dayFourButton.Bounds;
             mLabel = managerLabel.Bounds;
             fLabel = activitiesLabel.Bounds;
-            cLabel = caloriesBurnedLabel.Bounds;
             fTextBox = activitiesTextBox.Bounds;
-            cTextBox = caloriesBurnedTextBox.Bounds;
             iBox = inputBox.Bounds;
             mFont = managerLabel.Font;
             fFont = activitiesLabel.Font;
-            cFont = caloriesBurnedLabel.Font;
             sqlCommandList.Add("SELECT al.activities_list_id, users.user_name, activities.activities_name, activities.calories_burned\r\nFROM activities_list al\r\nJOIN users ON al.user_id = users.user_id\r\nJOIN activities ON al.activities_id = activities.activities_id\r\nWHERE al.date = current_date and al.user_id = @userID\r\nORDER BY al.activities_list_id;");
             sqlCommandList.Add("SELECT al.activities_list_id, users.user_name, activities.activities_name, activities.calories_burned\r\nFROM activities_list al\r\nJOIN users ON al.user_id = users.user_id\r\nJOIN activities ON al.activities_id = activities.activities_id\r\nWHERE al.date = current_date - 1 and al.user_id = @userID\r\nORDER BY al.activities_list_id;");
             sqlCommandList.Add("SELECT al.activities_list_id, users.user_name, activities.activities_name, activities.calories_burned\r\nFROM activities_list al\r\nJOIN users ON al.user_id = users.user_id\r\nJOIN activities ON al.activities_id = activities.activities_id\r\nWHERE al.date = current_date - 2 and al.user_id = @userID\r\nORDER BY al.activities_list_id;");
@@ -100,11 +97,8 @@ namespace SehatBank
             resize.resizeControl(daySeven, daySevenButton, originalSize, this.Width, this.Height);
             resize.resizeControl(mLabel, managerLabel, originalSize, this.Width, this.Height);
             resize.resizeControl(fLabel, activitiesLabel, originalSize, this.Width, this.Height);
-            resize.resizeControl(cLabel, caloriesBurnedLabel, originalSize, this.Width, this.Height);
             resize.resizeControl(fTextBox, activitiesTextBox, originalSize, this.Width, this.Height);
-            resize.resizeControl(cTextBox, caloriesBurnedTextBox, originalSize, this.Width, this.Height);
             resize.resizeControl(iBox, inputBox, originalSize, this.Width, this.Height);
-            resize.resizeFont(cFont, caloriesBurnedLabel, originalSize, this.Height);
             resize.resizeFont(fFont, activitiesLabel, originalSize, this.Height);
             resize.resizeFont(mFont, managerLabel, originalSize, this.Height);
         }
@@ -142,7 +136,11 @@ namespace SehatBank
 
         private void dataView_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-
+            if (e.RowIndex >= 0)
+            {
+                r = dataView.Rows[e.RowIndex];
+                activitiesTextBox.Text = r.Cells["activities_name"].Value.ToString();
+            }
         }
 
         private void dayOneButton_Click(object sender, EventArgs e)
@@ -285,6 +283,56 @@ namespace SehatBank
                 dt.Load(rd);
                 dataView.DataSource = dt;
                 con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "FAIL!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                dt = null;
+                sql = "select * from at_delete(:_activities_list_id)";
+                cmd = new NpgsqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("_activities_list_id", r.Cells["activities_list_id"].Value.ToString());
+                object result = cmd.ExecuteScalar();
+                if (result != null)
+                {
+                    MessageBox.Show("Delete Sukses");
+                    activitiesTextBox = null;
+                    r = null;
+                }
+                con.Close();
+                dayOneButton.PerformClick();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "FAIL!!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void editButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                sql = "select * from at_update(:_activities_list_id, :_activities_id)";
+                cmd = new NpgsqlCommand(sql, con);
+                cmd.Parameters.AddWithValue("_activities_list_id", r.Cells["activities_list_id"].Value.ToString());
+                cmd.Parameters.AddWithValue("_activities_id", Activities.GetActivitiesId(activitiesTextBox.Text).ToString());
+                object result = cmd.ExecuteScalar();
+                if (result != null)
+                {
+                    MessageBox.Show("Edit Sukses");
+                    activitiesTextBox = null;
+                    r = null;
+                }
+                con.Close();
+                dayOneButton.PerformClick();
             }
             catch (Exception ex)
             {
